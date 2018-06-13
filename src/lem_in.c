f3c9522a128ce2047ad89c239aca1f8563d7e2f6
WSeegers <wseegers.mauws@gmail.com>
2018-06-13 15:24:22 +0200

    posibilities list functioning

--- src/lem_in.c
+++ src/lem_in.c
@@ -6,7 +6,7 @@
 /*   By: wseegers <wseegers.mauws@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/11 09:50:57 by wseegers          #+#    #+#             */
-/*   Updated: 2018/06/13 11:26:13 by wseegers         ###   ########.fr       */
+/*   Updated: 2018/06/13 15:22:35 by wseegers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,61 @@
 #include "lem_in.h"
 #include "f_print.h"
 #include "s_list.h"
-#include "s_vertex.h"
+#include "s_graph.h"
+
+void	weigh_path(t_graph *path)
+{
+	size_t	i;
+	t_vert	*cur;
+	t_vert	*prev;
+
+	i = -1;
+	prev = NULL;
+	while (++i < path->size)
+	{
+		cur = s_vert_get(path, i);
+		if (prev)
+			s_vert_set_cost(prev, cur, 2);
+		prev = cur;
+	}
+}
+
+t_list	*get_paths(t_env *env)
+{
+	t_list	*paths;
+	t_vert	*start;
+	t_vert	*target;
+	t_graph	*shrt_path;
+	size_t 	i;
+
+	paths = s_list_create(NULL);
+	start = s_vert_get(env->map, env->start);
+	target = s_vert_get(env->map, env->target);
+	i = -1;
+	while (1)
+	{
+		shrt_path = dijkstra(start, target);
+		
+		weigh_path(shrt_path);
+
+		if (s_graph_equ(shrt_path, (t_graph*)s_list_get(paths, 0)))
+			break;
+		s_graph_print_verts(shrt_path);
+		f_printf("\n");
+		s_list_append(paths, shrt_path);
+		s_graph_reset_path(env->map);
+	}
+	return (paths);
+}
 
 int		main(void)
 {
 	t_env	*env;
-	t_vert	*target;
-	t_vert	*start;
-	t_vert	*temp;
-	size_t 	i;
+	t_list 	*paths;
 
 	env = parse_env();
-	start = s_vert_get(env->map->vert_list, env->start);
-	target = s_vert_get(env->map->vert_list, env->target);
-	f_printf("start: %s\n", start->name);
-	f_printf("target: %s\n", target->name);
-	f_printf("dijkstra started..");
-	dijkstra(start, target);
-	f_printf("dijkstra ended\n");
-	i = -1;
-	/*while (++i < env->map->vert_list->size)
-	{
-		temp = s_vert_get(env->map->vert_list, i);
-		if (temp->parent)
-			f_printf("target: %s -> parent: %s\n",temp->name, temp->parent->name);
-	}*/
-	f_printf(target->name);
-	temp = target->parent;
-	while (temp)
-	{
-		f_printf("<-%s",temp->name);
-		temp = temp->parent;
-	}
-	f_printf("\n");
-
+	paths = get_paths(env); 
+	//paths = get_best_paths(paths);
+	//issue_orders();
 	exit (0);
 }
--- src/lem_in.h
+++ src/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: wseegers <wseegers.mauws@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/11 09:50:24 by wseegers          #+#    #+#             */
-/*   Updated: 2018/06/13 11:12:38 by wseegers         ###   ########.fr       */
+/*   Updated: 2018/06/13 13:28:17 by wseegers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ typedef struct 	s_env
 	size_t		node_no;
 
 	t_lemings	*lemings;
-	int			lem_no;
+	size_t		lem_no;
 
 }				t_env;
 
